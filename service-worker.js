63cd5f0f298130144f3601d3504a4c55652baec7
Valter Barros <valterinside@gmail.com>
2024-05-16 23:00:13 -0300

    fix extension id
    Signed-off-by: Valter Barros <valterinside@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 import { setData } from './scripts/js/utils/index.js';
 
 chrome.action.onClicked.addListener(async (tab) => {
-  await chrome.tabs.create({ url: 'chrome-extension://majfibgeemjobijejfoecibealdlmpjc/popup.html', active: true });
+  await chrome.tabs.create({ url: `chrome-extension://${chrome.runtime.id}/popup.html`, active: true });
   setData('currentTabId', tab.id);
 });
